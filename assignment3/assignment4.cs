5cbd582e89fe54c8fba3b7b9f9992ef911f961ff
AishaRahim <aisha.rahim@controlqore.com>
2023-07-06 17:46:18 +0530

    4th assignment

---
+++ assignment3/assignment4.cs
@@ -0,0 +1,112 @@
+//Write a program to display the below details of a given input string
+Console.WriteLine("Enter a string:");
+string str = Console.ReadLine();
+
+//Length of the string
+Console.WriteLine("String length is " + str.Length);
+
+//Index of a particular character
+char[] strarray = str.ToCharArray();
+
+Console.WriteLine("Enter the character:");
+char ch = Convert.ToChar(Console.ReadLine());
+for (int i = 0; i < str.Length; i++)
+{
+   if (strarray[i] == ch)
+   {
+       Console.WriteLine("Index of " + ch + " is " + i);
+   }
+}
+
+//Reverse of the string
+string rev = "";
+for (int i = strarray.Length - 1; i > -1; i--)
+{
+   rev += strarray[i];
+}
+Console.WriteLine("Reverse of the string is " + rev);
+
+//Append Hai at the beginning of the given string
+string newstr = str.Insert(0, "Hai ");
+Console.WriteLine("New string is " + newstr);
+
+//Last character of the string
+Console.WriteLine("Last character of the string " + str.Last());
+
+//Replace ‘a’ by ‘_’
+string rep = str.Replace('a', '_');
+Console.WriteLine("After replacing " + rep);
+
+//Calculate occurrence of given character in a string.
+Console.WriteLine("Enter character to find occurence: ");
+char occ = Convert.ToChar(Console.ReadLine());
+int count = 0;
+foreach (char c in str)
+{
+   if (c == occ)
+   {
+       count += 1;
+   }
+}
+Console.WriteLine("occurrence of given character is " + count);
+
+//Write a program to do the below datetime calculation
+//Print the current date and time in different formats
+
+DateTime currentDateTime = DateTime.Now;
+Console.WriteLine(currentDateTime);
+
+string formattedDateTime1 = currentDateTime.ToString("MM/dd/yyyy HH:mm");
+Console.WriteLine(formattedDateTime1);
+
+string formattedDateTime2 = currentDateTime.ToString("dddd, dd MMMM yyyy HH:mm");
+Console.WriteLine(formattedDateTime2);
+
+string formattedDateTime3 = currentDateTime.ToString("dddd, dd MMMM yyyy hh:mm tt");
+Console.WriteLine(formattedDateTime3);
+
+
+//Find the difference between two dates
+DateTime date1 = new DateTime(2023, 7, 1);
+DateTime date2 = DateTime.Now;
+var diff = date2 - date1;
+Console.WriteLine("Difference in Days: {0}", diff.Days);
+Console.WriteLine("Difference in Hours: {0}", diff.Hours);
+Console.WriteLine("Difference in Miniutes: {0}", diff.Minutes);
+Console.WriteLine("Difference in Seconds: {0}", diff.Seconds);
+
+//Display Day, Month, Year of a given Date
+DateTime date1 = new DateTime(2023, 7, 1);
+string disp = date1.ToString("dddd MMMM, yyyy");
+Console.WriteLine(disp);
+
+//To a given Date, add 10 days
+DateTime date1 = new DateTime(2023, 7, 1);
+Console.WriteLine("Date before addition is " + date1);
+Console.WriteLine("Date after addition is " + date1.AddDays(10));
+
+
+//Single Calculate  method, Calculate out add, out sub, out mult, out div
+using System;
+namespace ConsoleApp67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc(20, 10, out float add, out float sub, out float mult, out float div);
+            Console.WriteLine("sum is: " +add );
+            Console.WriteLine("diff is: " + sub);
+            Console.WriteLine("product is: " + mult);
+            Console.WriteLine("quotient is: " + div);
+        }
+        public static void Calc(float x, float y, out float add, out float sub, out float mult, out float div )
+        {
+            add = x + y;
+            sub = x - y;
+            mult = x * y;
+            div = x / y;                      
+        }
+
+    }
+}
