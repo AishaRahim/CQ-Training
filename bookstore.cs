39fb975fb20ba0b17faa96a983f24d2f046737c5
AishaRahim <aisha.rahim@controlqore.com>
2023-07-07 14:01:16 +0530

    updated

--- bookstore.cs
+++ bookstore.cs
@@ -23,9 +23,20 @@ BookId int foreign key references Book(BookId),
 CustomerName varchar(50)
 )
 
+CREATE TABLE CustOrder
+(
+CustomerId int foreign key references Customer(CustomerId),
+AuthId int foreign key references Author(AuthId),
+BookId int foreign key references Book(BookId),
+CreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+
+
 drop table Author
 drop table Customer
 drop table Book
+drop table CustOrder
+
 
 INSERT INTO Author (Authname,BookName) VALUES('auth1','book1')
 INSERT INTO Author (Authname,BookName) VALUES('auth2','book2')
@@ -39,7 +50,12 @@ INSERT INTO Customer (BookId,CustomerName) VALUES('1','cust1')
 INSERT INTO Customer (BookId,CustomerName) VALUES('2','cust2')
 INSERT INTO Customer (BookId,CustomerName) VALUES('3','cust3')
 
+INSERT INTO CustOrder (BookId,AuthId,CustomerId) VALUES('1','1','1')
+INSERT INTO CustOrder (BookId,AuthId,CustomerId) VALUES('2','3','1')
+INSERT INTO CustOrder (BookId,AuthId,CustomerId) VALUES('3','2','1')
+
 
 SELECT * FROM Author
 SELECT * FROM Book
 SELECT * FROM Customer
+SELECT * FROM CustOrder
