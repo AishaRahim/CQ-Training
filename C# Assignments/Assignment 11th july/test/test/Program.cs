bd9f003f8d7cf546d1803893822b033703a51bb8
AishaRahim <aisha.rahim@controlqore.com>
2023-07-12 14:18:43 +0530

    first

---
+++ C# Assignments/Assignment 11th july/test/test/Program.cs
@@ -0,0 +1,8 @@
+﻿using test;
+Student st = new Student(1,"John",85);
+st.Display();
+Student st1 = new Student(2, "David", 74);
+st1.Display();
+Student st2 = new Student(3, "Thomas", 90);
+st2.Display();
+
---
+++ C# Assignments/Assignment 11th july/test/test/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public char Grade 
+        {
+            get
+            {
+                if (Mark >= 90)
+                {
+                    return 'A';
+                }
+                else if (Mark >= 80 && Mark < 90)
+                {
+                    return 'B';
+                }
+                else if (Mark >= 70 && Mark < 80)
+                {
+                    return 'C';
+                }
+                else
+                    return 'D';
+            }
+        }
+
+        public Student(int SId, string SName,int SMark) 
+        {
+            Id = SId;
+            Name = SName;
+            Mark = SMark;          
+        }
+        public void Display()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Mark);
+            Console.WriteLine(Grade);
+        }
+    }
+}
