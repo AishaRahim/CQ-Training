96a73e03c816f4449f8eb8e61de273857edd5511
AishaRahim <aisha.rahim@controlqore.com>
2023-07-19 15:25:45 +0530

    corrected

--- C# Assignments/Assignment 14th july-1/Employee1/Employee1/Program.cs
+++ C# Assignments/Assignment 14th july-1/Employee1/Employee1/Program.cs
@@ -1,19 +1,24 @@
 ﻿using Employee1;
 DateTime dateTime = new DateTime(2023,07,01);
-Employee ob = new Employee(1,"Vaishnav",dateTime , "Dev", 30000,'P');
+Employee ob = new Employee(1,"Vaishnav",dateTime , "Dev", 30000,'O');
 
 IBonusCalculator ibc = null;
 
 if (ob.JobType=='P')
 {
-    ibc = new PermanentEmployeeBonusCalulator();
+     ibc = new PermanentEmployeeBonusCalulator();
 }
 else if (ob.JobType=='C')
 {
     ibc = new ContractEmployeeBonusCalculator();
 }
+else
+{
+    Console.WriteLine("error");
+    
+}
 
-double Bonus = ibc.CalculateBonus(ob);
+double? Bonus = ibc?.CalculateBonus(ob);
 
 Console.WriteLine(ob.Name);
 Console.WriteLine("bonus is:"+Bonus);
