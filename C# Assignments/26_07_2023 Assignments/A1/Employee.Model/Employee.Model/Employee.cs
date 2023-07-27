0367824fcc3c66e79d6489ebf7159397d31150c5
AishaRahim <aisha.rahim@controlqore.com>
2023-07-27 15:29:24 +0530

    A1

---
+++ C# Assignments/26_07_2023 Assignments/A1/Employee.Model/Employee.Model/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class Employee
+    {
+        private static int empNo;
+        public string empID { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string EmployeeType { get; set; }
+
+        static Employee()
+        {
+            empNo = 1000;
+        }
+        public Employee(string name, double salary, string empType)
+        {
+            
+            Name = name;
+            Salary = salary;
+            EmployeeType = empType;
+            empID = "Emp" + empNo;
+            empNo++;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Total number of employees:"+(empNo - 1000));
+        }
+
+    }
+}
---
+++ C# Assignments/26_07_2023 Assignments/A1/Employee/Employee/Program.cs
@@ -0,0 +1,7 @@
+﻿using EmployeeModel;
+
+Employee emp = new Employee("John Doe",15000, "Permanent");
+Employee emp1 = new Employee("Liam Smith", 20000, "Contract");
+Employee emp2 = new Employee("Mary James", 15000, "Permanent");
+
+emp2.Display();
