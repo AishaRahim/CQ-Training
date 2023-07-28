076c9cf3d00699e708e587e2b6a1555d66450143
AishaRahim <aisha.rahim@controlqore.com>
2023-07-28 12:06:54 +0530

    A5

---
+++ C# Assignments/26_07_2023 Assignments/A5/NewList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+int[] arr = { 1, 2, 3, 4, 5 };
+
+List<int> ints = new List<int>();
+for (int i = 0; i < arr.Length; i++)
+{
+    int value = arr[i] * 2;
+    ints.Add(value);
+}
+
+Console.WriteLine("After processing:");
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
