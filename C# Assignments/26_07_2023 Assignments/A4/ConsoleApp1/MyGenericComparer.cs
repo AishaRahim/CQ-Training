9a54c626c3acd77118df561e5f09f5cbf9581a85
AishaRahim <aisha.rahim@controlqore.com>
2023-07-28 11:34:16 +0530

    A4

---
+++ C# Assignments/26_07_2023 Assignments/A4/ConsoleApp1/MyGenericComparer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class MyGenericComparer<T> where T : IComparable<T>
+{
+    public T Compare(T val1, T val2)
+    {
+        return val1.CompareTo(val2) >= 0 ? val1 : val2;
+    }
+}
---
+++ C# Assignments/26_07_2023 Assignments/A4/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿MyGenericComparer<int> comparer = new MyGenericComparer<int>();
+int largestInt = comparer.Compare(10, 20);
+Console.WriteLine("Largest integer: " + largestInt);
+
+MyGenericComparer<string> comparerString = new MyGenericComparer<string>();
+string largestString = comparerString.Compare("apple", "orange");
+Console.WriteLine("Largest string: " + largestString);
+    
+
