268f70e0126141da15c2ccea75168e69aef97dbd
AishaRahim <aisha.rahim@controlqore.com>
2023-08-07 15:08:36 +0530

    A1

--- Flutter/A1.dart
+++ Flutter/A1.dart
@@ -1,25 +1,26 @@
 import 'dart:io';
 
-void main(){
-stdout.writeln("Enter the string: ");
-String? str = stdin.readLineSync();
-bool flag = true; 
-int? len = str?.length;
-if(len!=null && str!=null)
+void main()
 {
-  for(int i=0; i < len/2 ; i++)
+  stdout.writeln("Enter the string: ");
+  String? str = stdin.readLineSync();
+  bool flag = true;
+  int? len = str?.length;
+  if(len!=null && str!=null)
   {
-    if(str[i]!=str[len-i-1]){
-      flag = false;
+    for(int i=0; i < len/2 ; i++)
+    {
+      if(str[i]!=str[len-i-1]){
+        flag = false;
+      }
     }
   }
-}
-if(flag)
-{
-  print("string is palindrome");
-} 
-else
-{
-  print("String is not palindrome");
-}
+  if(flag)
+  {
+    print("string is palindrome");
+  } 
+  else
+  {
+    print("String is not palindrome");
+  }
 }
