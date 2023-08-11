96f368ec59e20d3ef2e15464a0ada1d7bce5d869
AishaRahim <aisha.rahim@controlqore.com>
2023-08-11 10:05:29 +0530

    desc added

--- Flutter/A3/flutter_application_1/lib/main.dart
+++ Flutter/A3/flutter_application_1/lib/main.dart
@@ -12,11 +12,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('DISPLAY')),
-        body: const Center(child: Text('Hello World')),
-        //description yet to add
-      
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hello World',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'This is a simple Flutter app.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
       )
     );
   }
