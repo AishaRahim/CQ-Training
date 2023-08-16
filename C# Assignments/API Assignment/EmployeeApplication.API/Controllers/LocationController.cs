382719409bdedb310760e91afc74536c3629b42b
AishaRahim <aisha.rahim@controlqore.com>
2023-08-16 09:21:08 +0530

    employee databsase

---
+++ C# Assignments/API Assignment/EmployeeApplication.API/Controllers/LocationController.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeApplication.Model.Model;
+using EmployeeApplication.Repository.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApplication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LocationController : ControllerBase
+    {
+        private readonly EmployeeApplicationContext? dbContext;
+
+        public LocationController(EmployeeApplicationContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("AddLocation")]
+
+        public void post(string Name)
+        {
+            Location location = new Location();
+            location.Name = Name;
+            dbContext?.Add(location);
+            dbContext?.SaveChanges();
+        }
+
+        [HttpDelete("Delete")]
+
+        public void delete(int id)
+        {
+            Location location = new Location();
+            location.Id = id;
+            dbContext?.Remove(location);
+            dbContext?.SaveChanges();
+        }
+    }
+}
+
+
---
+++ C# Assignments/API Assignment/EmployeeApplication.Repository/Migrations/20230814131654_addtables.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeApplication.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    locationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Department_Location_locationId",
+                        column: x => x.locationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    departmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Department_departmentId",
+                        column: x => x.departmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_locationId",
+                table: "Department",
+                column: "locationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_departmentId",
+                table: "Employees",
+                column: "departmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
